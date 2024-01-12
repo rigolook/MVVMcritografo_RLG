1f3c92b76919ca3c1e03880c48466b7ab2701afc
rigolook <rixxormen@gmail.com>
2024-01-12 09:09:14 -0700

    first commit

---
+++ CritografoMVVM_RLG/CritografoMVVM_RLG/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CritografoMVVM_RLG.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CritografoMVVM_RLG
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new critografo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CritografoMVVM_RLG/CritografoMVVM_RLG/Vistas/critografo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CritografoMVVM_RLG.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class critografo : ContentPage
+    {
+        public critografo()
+        {
+            InitializeComponent();
+        }
+    }
+}
